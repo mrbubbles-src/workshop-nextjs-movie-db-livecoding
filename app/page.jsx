4fc479983aa5ee8bc1cad45b814651e0a2a9d341
mrbubbles-src <manuel_fahrenholz@web.de>
2025-04-16 10:06:02 +0200

    refactor: enhance heading styling on home page
    Updated the heading in the Home component to include additional styling for better visual emphasis.

--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,9 @@ import PopularNow from '@/components/PopularNow';
 export default function Home() {
   return (
     <>
-      <h1>Hallo Klasse ich bin die startseite</h1>
+      <h1 className="m-32 text-brand-highlight">
+        Hallo Klasse ich bin die startseite
+      </h1>
       <Herosection />
       <PopularNow type={'movies'} />
       <PopularNow type={'tvShows'} />
